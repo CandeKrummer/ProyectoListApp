8fad1e6752d613de3876ce508c13f366ab438dec
Candela Krummer <candelakrummer@gmail.com>
2022-06-07 17:28:15 -0300

    pongo manualmente la foreing key de listCategory

--- src/db/models/shoppingList.js
+++ src/db/models/shoppingList.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       ShoppingList.hasMany(models.ListedProduct)
-      ShoppingList.belongsTo(models.ShoppingListCategory)
+      ShoppingList.belongsTo(models.ShoppingListCategory, { foreignKey: "listCategoryId", })
     }
   }
   ShoppingList.init({
