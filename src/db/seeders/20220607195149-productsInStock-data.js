5844b183a01ad60ea13ade24827e101ab87b0ad5
Candela Krummer <candelakrummer@gmail.com>
2022-06-07 17:27:51 -0300

    actualizo data del stock

--- src/db/seeders/20220607195149-productsInStock-data.js
+++ src/db/seeders/20220607195149-productsInStock-data.js
@@ -4,28 +4,28 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     let productosListados = [
       {
-        shoppingListId: 4,
+        shoppingListId: 5,
         productId: 1,
         cantidad: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        shoppingListId: 4,
+        shoppingListId: 5,
         productId: 2,
         cantidad: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        shoppingListId: 4,
+        shoppingListId: 5,
         productId: 3,
         cantidad: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        shoppingListId: 4,
+        shoppingListId: 5,
         productId: 4,
         cantidad: 2,
         createdAt: new Date(),
