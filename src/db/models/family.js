6c51518eb8e718f721bde4b97344952b5d130412
CELINA RINALDI <44110736@comunidadort.edu.ar>
2022-06-13 19:46:21 -0300

    creo seed de familia

--- src/db/models/family.js
+++ src/db/models/family.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(200),
       allowNull: false,
     },
-    name: {
+    address: {
       type: DataTypes.STRING(200),
       allowNull: false,
     },
