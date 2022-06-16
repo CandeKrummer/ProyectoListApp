8ec3b790d56bc7f5f0a624aadff88ca534f0ef95
CELINA RINALDI <44110736@comunidadort.edu.ar>
2022-06-15 22:30:54 -0300

    testeo obtener alacena virtual

---
+++ test/getVirtualCupboardTest.js
@@ -0,0 +1,41 @@
+const { default: axios } = require('axios');
+const chai = require('chai');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+/* app.use(express.json())
+app.use(express.urlencoded({ extended: true })) */
+
+
+const { assert } = chai;
+describe('Get Virtual Cupboard', (done) => {
+    it('returns 201 if the virtual cupboard was found succesfully', () => {
+
+        axios({
+            method: 'get',
+            url: 'http://localhost:3000/family',
+            data: { name: "", brand: "", }
+        }).then(response => {
+            assert.equal(response.status, 201)
+            done()
+        }).catch(err => {
+            console.log(err.response.status)
+            done()
+        })
+    })
+    it('returns 422 if the family doesnt exist', () => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:3000/family',
+            data: { name: "", brand: "",  id: 3456}
+        }).then(response => {
+            assert.equal(response.status, 422)
+            done()
+        }).catch(err => {
+            assert.equal(err.response.data.message, "FAMILY_DOESNT_EXIST")
+            console.log(err.response.status + ' ' + err.response.data.message)
+            done()
+        })
+    })
+})
+
+
