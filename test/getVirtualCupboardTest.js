574982a34c09f51796511ce25bca5a0b09648eb7
Candela Krummer <candelakrummer@gmail.com>
2022-06-16 21:18:42 -0300

    Testing de obtener alacena virtual

--- test/getVirtualCupboardTest.js
+++ test/getVirtualCupboardTest.js
@@ -2,37 +2,63 @@ const { default: axios } = require('axios');
 const chai = require('chai');
 const chaiFetch = require('chai-fetch');
 chai.use(chaiFetch);
-/* app.use(express.json())
-app.use(express.urlencoded({ extended: true })) */
 
 
 const { assert } = chai;
-describe('Get Virtual Cupboard', (done) => {
-    it('returns 201 if the virtual cupboard was found succesfully', () => {
+describe('Get Virtual Cupboard', () => {
+    let famId;
+    it('returns 201 if the family was created', (done) => {
+        axios.post(
+            'http://localhost:3000/families',
+            {
+                name: "Garcia",
+                address: "Pellerman 42",
+                number: "4343-4343",
+                password: "garcia20"
+            }
+        ).then(response => {
+            assert.equal(response.status, 201)
+            famId = response.data.familyId
+            done()
+        }).catch(err => {
+            assert.equal(err.response.status, 422)
+            done()
+        })
+    })
 
+    it('returns 201 if the virtual cupboard was found succesfully', (done) => {
         axios({
             method: 'get',
-            url: 'http://localhost:3000/family',
-            data: { name: "", brand: "", }
+            url: 'http://localhost:3000/virtual-cupboard/' + famId,
         }).then(response => {
             assert.equal(response.status, 201)
             done()
         }).catch(err => {
-            console.log(err.response.status)
             done()
         })
     })
-    it('returns 422 if the family doesnt exist', () => {
+
+    it('returns 422 if the family doesnt exist', (done) => {
         axios({
             method: 'get',
-            url: 'http://localhost:3000/family',
-            data: { name: "", brand: "",  id: 3456}
+            url: 'http://localhost:3000/virtual-cupboard/' + -1,
         }).then(response => {
             assert.equal(response.status, 422)
             done()
         }).catch(err => {
             assert.equal(err.response.data.message, "FAMILY_DOESNT_EXIST")
-            console.log(err.response.status + ' ' + err.response.data.message)
+            done()
+        })
+    })
+
+    it('returns 200 if the family was deleted', (done) => {
+        axios.delete(
+            'http://localhost:3000/families/' + famId, {
+        }).then(response => {
+            assert.equal(response.status, 201)
+            done()
+        }).catch(err => {
+            assert.equal(err.response.status, 422)
             done()
         })
     })
